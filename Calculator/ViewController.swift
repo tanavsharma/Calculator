2628ab5c3d21e1ce49869581f50b3f95d3526dbc
tanavsharma <sharma.tanav95@gmail.com>
2021-10-01 14:42:28 -0400

    created empty variables to hold userInputs. updated functions to reflect new variables. created and tested the first operation (addition). working.

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -19,10 +19,15 @@ class ViewController: UIViewController {
     @IBOutlet weak var resultsLabel: UILabel!
     
     /* Initalizing the variable that will hold
-     * the users input. Setting it zero in the
+     * the users input. Setting it empty in the
      * start.*/
     
     var computations:String = ""
+    var first = ""
+    var second = ""
+    var function = ""
+    var userInput = ""
+    var results = 0.0
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -43,16 +48,19 @@ class ViewController: UIViewController {
     /* All Clear button, is going to reset all the
      * variables and textviews to zero or empty.*/
     @IBAction func allClear(_ sender: Any) {
+        userInput = ""
         computations = ""
         computationsLabel.text = ""
         resultsLabel.text = ""
     }
     
     @IBAction func plusMinus(_ sender: Any) {
-        //addtoComputations(value: <#T##String#>)
+        function = ""
+        addtoComputations(value: "-")
     }
     
     @IBAction func percent(_ sender: Any) {
+        //function = "%"
         addtoComputations(value: "%")
     }
     
@@ -63,6 +71,7 @@ class ViewController: UIViewController {
     @IBAction func backSpace(_ sender: Any) {
         if(!computations.isEmpty){
             computations.removeLast()
+            userInput.removeLast()
             computationsLabel.text = computations}
     }
     
@@ -70,64 +79,96 @@ class ViewController: UIViewController {
     // Calculator Operators
     
     @IBAction func divide(_ sender: Any) {
+        function = "÷"
         addtoComputations(value: "÷")
+        first = userInput
+        userInput = ""
     }
     
     @IBAction func multiply(_ sender: Any) {
+        function = "*"
         addtoComputations(value: "*")
+        first = userInput
+        userInput = ""
     }
     
     @IBAction func addition(_ sender: Any) {
+        function = "+"
         addtoComputations(value: "+")
+        first = userInput
+        userInput = ""
     }
     
     @IBAction func subtract(_ sender: Any) {
+        function = ""
         addtoComputations(value: "-")
+        first = userInput
+        userInput = ""
     }
     
     @IBAction func equals(_ sender: Any) {
+        second = userInput
+        var firstInput = 0.0
+        var secondInput = 0.0
+        firstInput = Double(first)!
+        secondInput = Double(second)!
+        
+        if(function == "+"){
+            results = firstInput + secondInput
+            resultsLabel.text = String(results)
+        }
         
     }
     
     
     //Calculator Numbers
     @IBAction func one(_ sender: Any) {
+        userInput += "1"
         addtoComputations(value: "1")
     }
     
     @IBAction func two(_ sender: Any) {
+        userInput += "2"
         addtoComputations(value: "2")
     }
     
     @IBAction func three(_ sender: Any) {
+        userInput += "3"
         addtoComputations(value: "3")
     }
     
     @IBAction func four(_ sender: Any) {
+        userInput += "4"
         addtoComputations(value: "4")
     }
     
     @IBAction func five(_ sender: Any) {
+        userInput += "5"
         addtoComputations(value: "5")
     }
     
     @IBAction func six(_ sender: Any) {
+        userInput += "6"
         addtoComputations(value: "6")
     }
     
     @IBAction func seven(_ sender: Any) {
+        userInput += "7"
         addtoComputations(value: "7")
     }
     
     @IBAction func eight(_ sender: Any) {
+        userInput += "8"
         addtoComputations(value: "8")
     }
     
     @IBAction func nine(_ sender: Any) {
+        userInput += "9"
         addtoComputations(value: "9")
     }
     
     @IBAction func zero(_ sender: Any) {
+        userInput += "0"
         addtoComputations(value: "0")
     }
     
